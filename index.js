cd6fb36286d2e2ac388ca7e1ac870f4fd7e92775
Erik Baauw <erik.baauw@xs4all.nl>
2025-01-01 23:03:52 +0100

    Happy new year

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // hb-nb-tools/index.js
 //
 // Homebridge NB Tools.
-// Copyright © 2017-2024 Erik Baauw. All rights reserved.
+// Copyright © 2017-2025 Erik Baauw. All rights reserved.
 
 /** Homebridge NB Tools.
   *
